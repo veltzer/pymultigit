ec69cbec938b59cdb6361a36846cf27f0a1a0ca0
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-29 20:27:20 +0200

    remove githugpages plugin for sphinx

--- scripts/sphinx.sh
+++ scripts/sphinx.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 python3 setup.py build_sphinx
-python3 setup.py upload_sphinx -r https://pypi.python.org/pypi
+#python3 setup.py upload_sphinx -r https://pypi.python.org/pypi
+python3 setup.py upload_sphinx
