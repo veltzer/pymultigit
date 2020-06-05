6e3a542a3ba0e88996098d65e7931c29b6f01d4f
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-06 00:52:29 +0300

    new template

--- setup.py
+++ setup.py
@@ -2,28 +2,29 @@ import setuptools
 
 setuptools.setup(
     # the first three fields are a must according to the documentation
-    name='pymultigit',
-    version='0.0.32',
+    name="pymultigit",
+    version="0.0.32",
     packages=[
         'pymultigit',
         'pymultigit.endpoints',
     ],
     # from here all is optional
-    description='pymultigit is a command to help you deal with multiple git repositories',
-    long_description='pymultigit is a command to help you deal with multiple git repositories',
-    author='Mark Veltzer',
-    author_email='mark.veltzer@gmail.com',
-    maintainer='Mark Veltzer',
-    maintainer_email='mark.veltzer@gmail.com',
+    description="pymultigit is a command to help you deal with multiple git repositories",
+    long_description="pymultigit is a command to help you deal with multiple git repositories",
+    long_description_content_type="text/x-rst",
+    author="Mark Veltzer",
+    author_email="mark.veltzer@gmail.com",
+    maintainer="Mark Veltzer",
+    maintainer_email="mark.veltzer@gmail.com",
     keywords=[
         'git',
         'python',
         'repositories',
         'multiple',
     ],
-    url='https://veltzer.github.io/pymultigit',
-    download_url='https://github.com/veltzer/pymultigit',
-    license='MIT',
+    url="https://veltzer.github.io/pymultigit",
+    download_url="https://github.com/veltzer/pymultigit",
+    license="MIT",
     platforms=[
         'python3',
     ],
@@ -44,8 +45,8 @@ setuptools.setup(
     ],
     data_files=[
     ],
-    entry_points={'console_scripts': [
+    entry_points={"console_scripts": [
         'pymultigit=pymultigit.endpoints.main:main',
     ]},
-    python_requires='>=3.5',
+    python_requires=">=3.5",
 )
