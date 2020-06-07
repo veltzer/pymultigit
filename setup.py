25aee513a5baf4837f692d265cf2f5c0da8eee23
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-07 17:52:00 +0300

    bring license back

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     ],
     url="https://veltzer.github.io/pymultigit",
     download_url="https://github.com/veltzer/pymultigit",
-    # license="MIT",
+    license="MIT",
     platforms=[
         'python3',
     ],
