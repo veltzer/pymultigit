2e54fa24bdd63f700614e507a6162939ae693610
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-20 16:00:10 +0200

    added local branch command

--- pymultigit/static.py
+++ pymultigit/static.py
@@ -1,4 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.36"
+VERSION_STR = "0.0.37"
 DESCRIPTION = "pymultigit is a command to help you deal with multiple git repositories"
 APP_NAME = "pymultigit"
+LOGGER_NAME = "pymultigit"
