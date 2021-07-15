06e2d8d7c251da72846a97ac3e7dc834c0c447fd
Mark Veltzer <mark.veltzer@gmail.com>
2021-07-15 12:12:22 +0300

    make git quiet

--- pymultigit/configs.py
+++ pymultigit/configs.py
@@ -28,7 +28,7 @@ class ConfigDebug(Config):
     )
     git_quiet = ParamCreator.create_bool(
         help_string="add --quiet when running git?",
-        default=False,
+        default=True,
     )
     stats = ParamCreator.create_bool(
         help_string="show statistics are the end?",
