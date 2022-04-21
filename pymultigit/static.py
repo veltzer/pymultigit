575fe0a3bac7d6c08ffe773a85add0fb4ccc3cb2
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-21 03:29:23 +0300

    added glob or folders feature

--- pymultigit/static.py
+++ pymultigit/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.48"
+VERSION_STR = "0.0.49"
 DESCRIPTION = "Help you deal with multiple git repositories"
 APP_NAME = "pymultigit"
 LOGGER_NAME = "pymultigit"
