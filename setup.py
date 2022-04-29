c688ee1f1f8ed73fc96b5f5154b648490c3523db
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-29 03:56:23 +0300

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pymultigit",
-    version="0.0.63",
+    version="0.0.64",
     packages=[
         'pymultigit',
     ],
