12fc73154183cf26349828bcd852d845bf8ed3a3
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-07 14:27:34 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -5,7 +5,12 @@ package_name = config.project.project_name
 console_scripts = [
     "pymultigit=pymultigit.main:main",
 ]
-
+dev_requires = [
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
+]
 install_requires = [
     "gitpython",
     "pyfakeuse",
@@ -13,7 +18,6 @@ install_requires = [
     "pylogconf",
     "venv-run",
 ]
-
 test_requires = [
     "pylint",
     "pytest",
@@ -22,13 +26,7 @@ test_requires = [
     "pymakehelper",
 ]
 
-dev_requires = [
-    "pyclassifiers",
-    "pypitools",
-    "pydmt",
-    "Sphinx",
-]
+python_requires = ">=3.10"
 
-python_requires = ">=3.9"
-test_os = ["ubuntu-20.04"]
-test_python = ["3.9"]
+test_os = ["ubuntu-22.04"]
+test_python = ["3.10"]
