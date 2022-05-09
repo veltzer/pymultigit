2142ed57f13be84f9532de0b5c860e6c454b13b7
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-09 09:30:47 +0300

    new version

--- pymultigit/static.py
+++ pymultigit/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.67"
+VERSION_STR = "0.0.68"
 DESCRIPTION = "Help you deal with multiple git repositories"
 APP_NAME = "pymultigit"
 LOGGER_NAME = "pymultigit"
