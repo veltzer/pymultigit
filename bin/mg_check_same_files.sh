f2a826dc35f86a3fa79922a52cc2e7a92279d9da
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-30 18:15:29 +0300

    better conformity

--- bin/mg_check_same_files.sh
+++ bin/mg_check_same_files.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 # ls -d py* | wc -l
-pycmdtools mcmp --print "MANIFEST.in" py*/MANIFEST.in
-pycmdtools mcmp --print "setup.cfg" py*/setup.cfg
-pycmdtools mcmp --print "Makefile" py*/Makefile
+pycmdtools mcmp --print "py*/MANIFEST.in" py*/MANIFEST.in
+pycmdtools mcmp --print "py*/setup.cfg" py*/setup.cfg
+pycmdtools mcmp --print "py*/Makefile" py*/Makefile
+pycmdtools mcmp --print "py*/templates/README.md.mako" py*/templates/README.md.mako
+
 pycmdtools mcmp --print ".pylintrc" ./*/.pylintrc
 pycmdtools mcmp --print ".flake8" ./*/.flake8
 pycmdtools mcmp --print ".gitignore" ./*/.gitignore
@@ -11,7 +13,6 @@ pycmdtools mcmp --print ".shellcheckrc" ./*/.shellcheckrc
 pycmdtools mcmp --print "templates/LICENSE.mako" py*/templates/LICENSE.mako
 pycmdtools mcmp --print "templates/setup.py.mako" py*/templates/setup.py.mako
 pycmdtools mcmp --print "templates/README.rst.mako" py*/templates/README.rst.mako
-pycmdtools mcmp --print "py*/templates/README.md.mako" py*/templates/README.md.mako
 pycmdtools mcmp --print "[^py]*/templates/README.md.mako" [^py]*/templates/README.md.mako
 pycmdtools mcmp --print "templates/[pkg]/static.py.mako" py*/templates/*/static.py.mako
 pycmdtools mcmp --print "templates/.github/workflows/build.yml.mako" ./*/templates/.github/workflows/build.yml.mako
