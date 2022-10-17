6ccc21ca50a0cd58b3f27bc50bd540d401d8d831
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-17 03:34:33 +0300

    no commit message given

--- src/mg_check_same_files.sh
+++ src/mg_check_same_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # ls -d py* | wc -l
-pycmdtools mcmp --print "py*/MANIFEST.in" py*/MANIFEST.in
+# pycmdtools mcmp --print "py*/MANIFEST.in" py*/MANIFEST.in
 pycmdtools mcmp --print "py*/setup.cfg" py*/setup.cfg
 pycmdtools mcmp --print "py*/Makefile" py*/Makefile
 pycmdtools mcmp --print "py*/templates/README.md.mako" py*/templates/README.md.mako
@@ -17,7 +17,7 @@ pycmdtools mcmp --print "templates/README.rst.mako" py*/templates/README.rst.mak
 pycmdtools mcmp --print "[^py]*/templates/README.md.mako" [^py]*/templates/README.md.mako
 pycmdtools mcmp --print "templates/[pkg]/static.py.mako" py*/templates/*/static.py.mako
 pycmdtools mcmp --print "templates/.github/workflows/build.yml.mako" ./*/templates/.github/workflows/build.yml.mako
-pycmdtools mcmp --print "templates/requirements.txt.mako" ./*/templates/requirements.txt.mako
+# pycmdtools mcmp --print "templates/requirements.txt.mako" ./*/templates/requirements.txt.mako
 # configs
 pycmdtools mcmp --print "config/__init__.py" py*/config/__init__.py
 # .idea stuff
