403342de808d1d5cbfda5fc3f2a2c5fd57dda30b
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-14 02:29:52 +0300

    better script

--- src/mg_check_same_files.sh
+++ src/mg_check_same_files.sh
@@ -1,36 +1,71 @@
 #!/bin/bash -e
-# my repos
-# FOLDERS=$(echo */.veltzer.tag)
-# echo "$FOLDERS"
-# exit 1
+
+tags=(*/.veltzer.tag)
+folders=()
+for elem in "${tags[@]}"
+do
+	folders+=("${elem%/*}")
+done
+# printf "%s\n" "${folders[@]}"
+
+notags=([^py]*/.veltzer.tag)
+nofolders=()
+for elem in "${notags[@]}"
+do
+	nofolders+=("${elem%/*}")
+done
+# printf "%s\n" "${nofolders[@]}"
+
+pytags=(py*/.veltzer.tag)
+pyfolders=()
+for elem in "${pytags[@]}"
+do
+	pyfolders+=("${elem%/*}")
+done
+# printf "%s\n" "${pyfolders[@]}"
+
+function mcmp() {
+	local print=$1
+	local -n arr=$2
+	local add=$3
+	t=()
+	for elem in "${arr[@]}"
+	do
+		if [ -f "${elem}${add}" ]
+		then
+			t+=("${elem}${add}")
+		fi
+	done
+	# shellcheck disable=SC2046
+	pycmdtools mcmp --print "${print}" $(printf "%s " "${t[@]}")
+}
 # python modules
-pycmdtools mcmp --print "py*/setup.cfg" py*/setup.cfg
-pycmdtools mcmp --print "py*/Makefile" py*/Makefile
-pycmdtools mcmp --print "./*/.mypy.ini" ./*/.mypy.ini
-pycmdtools mcmp --print "./*/.pylintrc" ./*/.pylintrc
-pycmdtools mcmp --print "./*/.flake8" ./*/.flake8
-pycmdtools mcmp --print "./*/.gitignore" ./*/.gitignore
-pycmdtools mcmp --print "./*/.shellcheckrc" ./*/.shellcheckrc
+mcmp "py*/setup.cfg" pyfolders "/setup.cfg"
+mcmp "py*/Makefile" pyfolders "/Makefile"
+mcmp "./*/.mypy.ini" folders "/.mypy.ini"
+mcmp "./*/.pylintrc" folders "/.pylintrc"
+mcmp "./*/.flake8" folders "/.flake8"
+mcmp "./*/.gitignore" folders "/.gitignore"
+mcmp "./*/.shellcheckrc" folders "/.shellcheckrc"
 # templates
-pycmdtools mcmp --print "[^py]*/templates/README.md.mako" [^py]*/templates/README.md.mako
-pycmdtools mcmp --print "py*/templates/LICENSE.mako" py*/templates/LICENSE.mako
-pycmdtools mcmp --print "py*/templates/setup.py.mako" py*/templates/setup.py.mako
-pycmdtools mcmp --print "py*/templates/README.rst.mako" py*/templates/README.rst.mako
-pycmdtools mcmp --print "py*/templates/README.md.mako" py*/templates/README.md.mako
-pycmdtools mcmp --print "py*/templates/[pkg]/static.py.mako" py*/templates/*/static.py.mako
-pycmdtools mcmp --print "./*/templates/.github/workflows/build.yml.mako" ./*/templates/.github/workflows/build.yml.mako
-pycmdtools mcmp --print "./*/templates/requirements.txt.mako" ./*/templates/requirements.txt.mako
+mcmp "[^py]*/templates/README.md.mako" nofolders /templates/README.md.mako
+mcmp "py*/templates/LICENSE.mako" pyfolders /templates/LICENSE.mako
+mcmp "py*/templates/setup.py.mako" pyfolders /templates/setup.py.mako
+mcmp "py*/templates/README.rst.mako" pyfolders /templates/README.rst.mako
+mcmp "py*/templates/README.md.mako" pyfolders /templates/README.md.mako
+mcmp "./*/templates/.github/workflows/build.yml.mako" folders /templates/.github/workflows/build.yml.mako
+mcmp "./*/templates/requirements.txt.mako" folders /templates/requirements.txt.mako
 # .idea stuff
-pycmdtools mcmp --print "py*/.idea/.gitignore" py*/.idea/.gitignore
+mcmp "py*/.idea/.gitignore" pyfolders /.idea/.gitignore
 # results of templates
-pycmdtools mcmp --print "py*/.github/workflows/build.yml" py*/.github/workflows/build.yml
-pycmdtools mcmp --print "[^py]*/.github/workflows/build.yml" [^py]*/.github/workflows/build.yml
+mcmp "py*/.github/workflows/build.yml" pyfolders /.github/workflows/build.yml
+mcmp "[^py]*/.github/workflows/build.yml" nofolders /.github/workflows/build.yml
 # github stuff
-pycmdtools mcmp --print "./*/.github/FUNDING.yml" ./*/.github/FUNDING.yml
+mcmp "./*/.github/FUNDING.yml" folders /.github/FUNDING.yml
 # sphinx
-pycmdtools mcmp --print "./*/sphinx/conf.py" ./*/sphinx/conf.py
-pycmdtools mcmp --print "./*/sphinx/index.rst" ./*/sphinx/index.rst
+mcmp "./*/sphinx/conf.py" folders /sphinx/conf.py
+mcmp "./*/sphinx/index.rst" folders /sphinx/index.rst
 # config
-pycmdtools mcmp --print "./*/config/platform.py" ./*/config/platform.py
-pycmdtools mcmp --print "./*/config/personal.py" ./*/config/personal.py
-pycmdtools mcmp --print "./*/config/github.py" ./*/config/github.py
+mcmp "./*/config/platform.py" folders /config/platform.py
+mcmp "./*/config/personal.py" folders /config/personal.py
+mcmp "./*/config/github.py" folders /config/github.py
