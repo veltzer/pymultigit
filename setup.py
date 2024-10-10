2551f08f703f59335ee6c626b9bd9f765b4b08a3
Mark Veltzer <mark.veltzer@gmail.com>
2024-10-10 22:15:48 +0300

    new version 0.92

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pymultigit",
-    version="0.0.91",
+    version="0.0.92",
     packages=[
         "pymultigit",
         "pymultigit.utils",
