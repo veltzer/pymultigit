e93dee163c4d4e5c817a73802cf2f437ce557f49
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-20 04:06:36 +0200

    removed dev envs

--- src/mg_find_ide_files.sh
+++ src/mg_find_ide_files.sh
@@ -3,4 +3,5 @@ find .\
 	-name ".idea" -or\
 	-name ".project" -or\
 	-name ".settings" -or\
-	-name "build.properties"
+	-name ".classpath" -or\
+	-name "build.properties" | grep -v gems | grep -v node_modules
