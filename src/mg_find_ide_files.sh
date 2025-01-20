89898b0f83887660c8501ecc43f0f0cd0789766d
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-20 04:22:51 +0200

    no commit message given

--- src/mg_find_ide_files.sh
+++ src/mg_find_ide_files.sh
@@ -2,6 +2,8 @@
 find .\
 	-name ".idea" -or\
 	-name ".project" -or\
+	-name ".project.xml" -or\
 	-name ".settings" -or\
 	-name ".classpath" -or\
+	-name ".cache" -or\
 	-name "build.properties" | grep -v gems | grep -v node_modules
