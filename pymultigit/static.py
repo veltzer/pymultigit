23a54c704bf030bcbe1c2eef20df0ba94078001b
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:33:48 +0300

    fixed up deps

--- pymultigit/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.93"
-DESCRIPTION = "Pymultigit helps you deal with multiple git repositories"
-APP_NAME = "pymultigit"
-LOGGER_NAME = "pymultigit"
