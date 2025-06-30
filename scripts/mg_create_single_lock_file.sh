f5edb93e8973f0073885e00071e4a64d3ec35204
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 21:03:49 +0300

    no commit message given

--- scripts/mg_create_single_lock_file.sh
+++ scripts/mg_create_single_lock_file.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -eu
 
-rm -rf /tmp/venv
-virtualenv /tmp/venv
+if [ ! -f "requirements.thawed.txt" ]
+then
+	echo "no thawed requirements file"
+	exit 1
+fi
+rm -rf "/tmp/venv"
+virtualenv "/tmp/venv"
 # shellcheck source=/dev/null
-source /tmp/venv/bin/activate
-pip install -r requirements.thawed.txt
-pip freeze > requirements.txt
+source "/tmp/venv/bin/activate"
+pip install -r "requirements.thawed.txt"
+pip freeze > "requirements.txt"
 deactivate
-rm -rf /tmp/venv
+rm -rf "/tmp/venv"
