f1e61289a1846877ccda22e7687cf74c97cbfbed
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 23:59:03 +0300

    no commit message given

--- scripts/mg_all_create_lock.sh
+++ scripts/mg_all_create_lock.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -eu
 
-env_path="/tmp/venv_all"
+function old_code() {
+	env_path="/tmp/venv_all"
+	# this is the old code
+	rm -rf "${env_path}"
+	virtualenv "${env_path}"
+	# shellcheck source=/dev/null
+	source "${env_path}/bin/activate"
+	pip install -r "requirements.thawed.txt"
+	pip freeze > "requirements.txt"
+	deactivate
+	rm -rf "${env_path}"
+}
 
 for x in * 
 do
@@ -14,13 +25,6 @@ do
 	fi
 	cd "${x}"
 	echo "doing [${x}]"
-	rm -rf "${env_path}"
-	virtualenv "${env_path}"
-	# shellcheck source=/dev/null
-	source "${env_path}/bin/activate"
-	pip install -r "requirements.thawed.txt"
-	pip freeze > "requirements.txt"
-	deactivate
-	rm -rf "${env_path}"
+	uv pip compile "requirements.thawed.txt" --no-annotate --no-header --output-file "requirements.txt"
 	cd ..
 done
