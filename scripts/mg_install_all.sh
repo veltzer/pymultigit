2a3084035a88e8ee77ae38dcd87d3e494ecef5c1
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 20:51:48 +0300

    new script

---
+++ scripts/mg_install_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+for x in * 
+do
+	if [ ! -d "${x}" ]
+	then
+		continue
+	fi
+	cd "${x}"
+	if [ -f "requirements.txt" ]
+	then
+		pip install -r "requirements.txt"
+	fi
+	cd ..
+done
