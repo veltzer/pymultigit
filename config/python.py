bb2dbd29b0f52fdf9363a90791c27d012dcf4616
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:16 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -1,12 +1,7 @@
 """ python deps for this project """
 
-scripts: dict[str,str] = {
-    "pymultigit": "pymultigit.main:main",
-}
+import config.shared
 
-config_requires: list[str] = [
-    "pyclassifiers",
-]
 install_requires: list[str] = [
     "gitpython",
     "pyfakeuse",
@@ -14,16 +9,11 @@ install_requires: list[str] = [
     "pylogconf",
     "venv-run",
 ]
-build_requires: list[str] = [
-    "hatch",
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-]
-test_requires: list[str] = [
-    "pylint",
-    "pytest",
-    "mypy",
-    "ruff",
-]
-requires = config_requires + install_requires + build_requires + test_requires
+
+build_requires: list[str] = config.shared.PBUILD
+test_requires: list[str] = config.shared.PTEST
+requires = install_requires + build_requires + test_requires
+
+scripts: dict[str,str] = {
+    "pymultigit": "pymultigit.main:main",
+}
