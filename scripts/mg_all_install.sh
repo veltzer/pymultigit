3863ac8eaeaad3b2270af202b28fa70444929645
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-02 10:04:24 +0300

    no commit message given

--- scripts/mg_all_install.sh
+++ scripts/mg_all_install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash -eu
 
-for x in * 
-do
-	if [ ! -d "${x}" ]
-	then
-		continue
-	fi
-	echo "doing [${x}]"
-	cd "${x}"
-	if [ -f "requirements.txt" ]
-	then
-		uv pip install -r "requirements.txt"
-	fi
-	cd ..
-done
+function old_code {
+	for x in * 
+	do
+		if [ ! -d "${x}" ]
+		then
+			continue
+		fi
+		echo "doing [${x}]"
+		cd "${x}"
+		if [ -f "requirements.txt" ]
+		then
+			uv pip install -r "requirements.txt"
+		fi
+		cd ..
+	done
+}
+
+# create full requirements file
+cat ./*/requirements.thawed.txt | sort -u > "/tmp/reqs"
+uv cache clean
+uv pip install -r "/tmp/reqs"
