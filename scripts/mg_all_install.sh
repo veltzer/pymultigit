d9f8377d4dc640ae3a4ec7d6697fc649adf0543b
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-06 10:44:08 +0300

    no commit message given

--- scripts/mg_all_install.sh
+++ scripts/mg_all_install.sh
@@ -19,6 +19,7 @@ function old_code {
 
 # create full requirements file
 cat ./*/requirements.thawed.txt | sort -u > "/tmp/reqs"
+# cat ./*/requirements.txt | sort -u > "/tmp/reqs"
 uv cache clean
 uv pip install -r "/tmp/reqs"
 uv pip freeze > /tmp/master-constraints.txt
