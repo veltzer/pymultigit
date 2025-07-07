622c28b7a11d6ed2dd20babe27b67a32fe29fd28
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-08 01:25:26 +0300

    no commit message given

--- scripts/mg_all_install.sh
+++ scripts/mg_all_install.sh
@@ -1,41 +1,16 @@
 #!/bin/bash -eu
 
-function old_code {
-	for x in * 
-	do
-		if [ ! -d "${x}" ]
-		then
-			continue
-		fi
-		echo "doing [${x}]"
-		cd "${x}"
-		if [ -f "requirements.txt" ]
-		then
-			uv pip install -r "requirements.txt"
-		fi
-		cd ..
-	done
-}
-
-# create full requirements file
-cat ./*/requirements.thawed.txt | sort -u > "/tmp/reqs"
-# cat ./*/requirements.txt | sort -u > "/tmp/reqs"
-uv cache clean
-uv pip install -r "/tmp/reqs"
-uv pip freeze > /tmp/master-constraints.txt
-
 for x in * 
 do
 	if [ ! -d "${x}" ]
 	then
 		continue
 	fi
-	if [ ! -f "${x}/requirements.thawed.txt" ]
-	then
-		continue
-	fi
-	cd "${x}"
 	echo "doing [${x}]"
-	uv pip compile "requirements.thawed.txt" --constraint /tmp/master-constraints.txt --no-annotate --no-header > "requirements.txt"
+	cd "${x}"
+	if [ -f "requirements.txt" ]
+	then
+		uv pip install -r "requirements.txt"
+	fi
 	cd ..
 done
