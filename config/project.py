e3559f7df260ebc47e2714782ead838e92967f06
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:22 +0300

    pydmt -> rsconstruct

--- config/project.py
+++
@@ -1,9 +0,0 @@
-""" project definitions """
-name = "pymultigit"
-description_short = "Pymultigit helps you deal with multiple git repositories"
-keywords = [
-    "git",
-    "python",
-    "repositories",
-    "multiple",
-]
